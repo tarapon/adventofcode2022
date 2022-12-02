7b5b8aad8f3cc64a1768134d51c08989d3506e22
Ivan Tarapon <tarapon@gmail.com>
2022-12-02 14:34:27 +0100

    complete round 2b

---
+++ day2/main_b.rb
@@ -0,0 +1,82 @@
+class Game
+  def initialize
+    @player1 = 0
+    @player2 = 0
+  end
+
+  def play(move_1, result)
+    move_1 = to_move(move_1)
+    move_2 = determine_move(move_1, result)
+
+    points = play_round(move_1, move_2)
+    @player1 += move_score(move_1) + points[0]
+    @player2 += move_score(move_2) + points[1]
+  end
+
+  def score
+    [@player1, @player2]
+  end
+
+  private
+
+  MOVES = {
+    # original => [loose win]
+    "rock" => %w[paper scissors],
+    "paper" => %w[scissors rock],
+    "scissors" => %w[rock paper]
+  }
+
+  def determine_move(move1, result)
+    case result
+    when "X" # loose
+      MOVES[move1][1]
+    when "Y" # draw
+      move1
+    when "Z" # win
+      MOVES[move1][0]
+    end
+  end
+
+  def to_move(code)
+    case code
+    when "A", "X" # rock
+      'rock'
+    when "B", "Y" # paper
+      'paper'
+    when "C", "Z" # scissors
+      'scissors'
+    else
+      raise "Invalid move"
+    end
+  end
+
+  def move_score(move)
+    case move
+    when "rock"
+      1
+    when "paper"
+      2
+    when "scissors"
+      3
+    end
+  end
+
+  def play_round(move_1, move_2)
+    return [3, 3] if move_1 == move_2
+
+    return [6, 0] if move_1 == "rock" && move_2 == "scissors"
+    return [6, 0] if move_1 == "scissors" && move_2 == "paper"
+    return [6, 0] if move_1 == "paper" && move_2 == "rock"
+
+    [0, 6]
+  end
+end
+
+g = Game.new
+
+File.readlines('day2/input.txt').map do |line|
+  move_a, move_b = line.split(' ')
+  g.play(move_a, move_b)
+end
+
+puts g.score.inspect
