8f02f89904e63cda291a9ce218e51d1d828c8e23
Ivan Tarapon <tarapon@gmail.com>
2022-12-04 08:39:04 +0100

    refactor day 3

--- day3/main.rb
+++ day3/main.rb
@@ -1,21 +1,19 @@
 def priority(char)
-  if char >= 'a'
-    char.codepoints.first - 96
-  else
-    char.codepoints.first - 38
-  end
+  char >= 'a' ? char.ord - 96 : char.ord - 38
 end
 
-res = File.readlines('day3/input.txt').map do |line|
+input = File.readlines('day3/input.txt').map(&:strip)
+
+res = input.map do |line|
   a, b = line.split('').each_slice(line.size / 2).to_a
-  priority(a.intersection(b).first)
+  priority((a & b).first)
 end
 
-puts "a: ", res.inspect, res.sum
+puts "a: ", res.sum
 
-res = File.readlines('day3/input.txt').map(&:strip).each_slice(3).map do |group|
+res = input.each_slice(3).map do |group|
   a, b, c = group.map { |l| l.split('') }
-  priority(a.intersection(b).intersection(c).first)
+  priority((a & b & c).first)
 end
 
-puts "b: ", res.inspect, res.sum
+puts "b: ", res.sum
