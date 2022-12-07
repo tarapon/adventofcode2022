4dd2179cab537a70650991d589cb8895eee0cc6a
Ivan Tarapon <tarapon@gmail.com>
2022-12-07 09:56:18 +0100

    complete day 7 part b

--- day7/main.rb
+++ day7/main.rb
@@ -55,27 +55,21 @@ File.readlines('day7/input.txt')[1..].each do |line|
   next if line.match(%r{^\$ ls})
 
   if line.match(%r{^\$ cd \.\.})
-    puts "cd .."
     stack.pop
     next
   end
 
   if (m = line.match(%r{^\$ cd (.+)}))
-    puts "cd #{m[1]}"
     stack.push(stack.last.get_child(m[1]))
     next
   end
 
   if (m = line.match(%r{^dir (.+)}))
-    puts "add dir #{m[1]} to #{stack.last.name}"
-
     stack.last.add_child(ElfDir.new(m[1]))
     next
   end
 
   if (m = line.match(%r{(\d+) (.+)}))
-    puts "add file #{m[2]} to #{stack.last.name}"
-
     stack.last.add_child(ElfFile.new(m[2], m[1].to_i))
     next
   end
@@ -84,3 +78,7 @@ end
 root = stack.first
 
 puts "a: ", root.filter(&:dir?).map(&:size).filter { |size| size <= 100_000 }.sum
+
+space_needed = 30_000_000 - 70_000_000 + root.size
+
+puts "b: ", root.filter { |f| f.dir? && f.size >= space_needed }.min_by(&:size)&.size
