90f2d7c3fc00af8fb1bd862662ae70d8387e4484
Ivan Tarapon <tarapon@gmail.com>
2022-12-13 09:43:23 +0100

    complete day 9 part b

--- day9/main.rb
+++ day9/main.rb
@@ -1,48 +1,68 @@
 class Snake
-  attr_reader :head, :tail
   attr_reader :trail
 
-  def initialize
-    @head = [0, 0]
-    @tail = [0, 0]
-    @trail = [@tail.dup]
+  def initialize(size)
+    @snake = (size + 1).times.map { [0, 0] }
+    @trail = [tail.dup]
+  end
+
+  def head
+    @snake.first
+  end
+
+  def tail
+    @snake.last
   end
 
   def move(dir, steps)
-    case dir
-    when 'U'
-      head[1] += steps
-    when 'D'
-      head[1] -= steps
-    when 'L'
-      head[0] -= steps
-    when 'R'
-      head[0] += steps
-    end
+    steps.times do
+      case dir
+      when 'U'
+        head[1] += 1
+      when 'D'
+        head[1] -= 1
+      when 'L'
+        head[0] -= 1
+      when 'R'
+        head[0] += 1
+      end
 
-    pull_up until touching?
+      pull_up_all
+    end
   end
 
   private
 
-  def touching?
-    (head[0] - tail[0]).abs <= 1 && (head[1] - tail[1]).abs <= 1
+  def pull_up_all
+    @snake.each_cons(2) do |a, b|
+      break if touching?(b, a)
+      pull_up(b, a)
+    end
+
+    trail << tail.dup unless tail == trail.last
   end
 
-  def pull_up
-    tail[0] += (head[0] <=> tail[0])
-    tail[1] += (head[1] <=> tail[1])
+  def touching?(a, b)
+    (b[0] - a[0]).abs <= 1 && (b[1] - a[1]).abs <= 1
+  end
 
-    trail << tail.dup
+  def pull_up(a, b)
+    a[0] += (b[0] <=> a[0])
+    a[1] += (b[1] <=> a[1])
   end
 end
 
-s = Snake.new
+def simulate(n)
+  s = Snake.new(n)
 
-File.readlines('day9/input.txt').each do |line|
-  dir, steps = line.split(' ')
-  s.move(dir, steps.to_i)
+  File.readlines('day9/input.txt').each do |line|
+    dir, steps = line.split(' ')
+    s.move(dir, steps.to_i)
+  end
+
+  s.trail
 end
 
-puts "a: ", s.trail.uniq.size
+puts "a: ", simulate(1).uniq.size
+puts "b: ", simulate(9).uniq.size
 
